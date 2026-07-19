4c34f40558b48061fd79ff2bdf984ff2d31ad03a
moizdiv9019 <moiz9019455456@gmail.com>
2026-07-19 14:44:04 +0530

    created a new function in db.py called Get data base config to make thing easier and now we are loadin the data confing from dotenv file

--- backend/db.py
+++ backend/db.py
@@ -1,20 +1,26 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
 
+def Get_data_base_url():
+      load_dotenv()
+      data_base_config_string=os.environ.get("Data_Base_config")
+      if data_base_config_string:
+          Data_Base_config_dict=eval(f"dict({data_base_config_string})")
+          return(True,Data_Base_config_dict)
+      else:
+          return (False,"unable to load data_base_config_string")
 
 def check_database_is_active()->bool:
       try:  
         # Establishing the connection
-        db_connection = mysql.connector.connect(
-            host="localhost",      # Standard for XAMPP
-            user="root",           # Default XAMPP username
-            password="ms9019",           # Default XAMPP password is empty
-            database="To_do",
-            auth_plugin="mysql_native_password" # Replace with your actual database name
-        )
-    
-        if db_connection.is_connected():
-            return True ,db_connection
-            
+        Goted,data_base_url=Get_data_base_url()
+        if Goted:
+            db_connection = mysql.connector.connect(**data_base_url)
+
+            if db_connection.is_connected():
+                return True ,db_connection
+                
     
       except mysql.connector.Error as err:
           return False ,err
--- backend/test.py
+++ backend/test.py
@@ -1,46 +1,80 @@
-import mysql.connector
+# import mysql.connector
 
-# try:
-#     # Establishing the connection
-#     db_connection = mysql.connector.connect(
-#         host="localhost",      # Standard for XAMPP
-#         user="root",           # Default XAMPP username
-#         password="ms9019",           # Default XAMPP password is empty
-#         database="app_company",
-#         auth_plugin="mysql_native_password" # Replace with your actual database name
-#     )
+# # try:
+# #     # Establishing the connection
+# #     db_connection = mysql.connector.connect(
+# #         host="localhost",      # Standard for XAMPP
+# #         user="root",           # Default XAMPP username
+# #         password="ms9019",           # Default XAMPP password is empty
+# #         database="app_company",
+# #         auth_plugin="mysql_native_password" # Replace with your actual database name
+# #     )
 
-#     if db_connection.is_connected():
-#         print("Successfully connected to the database")
+# #     if db_connection.is_connected():
+# #         print("Successfully connected to the database")
         
-#         # Creating a cursor object to perform queries
-#         cursor = db_connection.cursor()
+# #         # Creating a cursor object to perform queries
+# #         cursor = db_connection.cursor()
         
-#         # Example query: Fetching data
-#         cursor.execute("SELECT DATABASE();")
-#         record = cursor.fetchone()
-#         print(f"You're connected to database: {record}")
+# #         # Example query: Fetching data
+# #         cursor.execute("SELECT DATABASE();")
+# #         record = cursor.fetchone()
+# #         print(f"You're connected to database: {record}")
 
-# except mysql.connector.Error as err:
-#     print(f"Error: {err}")
+# # except mysql.connector.Error as err:
+# #     print(f"Error: {err}")
 
-# finally:
-#     # Always close the connection
-#     if 'db_connection' in locals() and db_connection.is_connected():
-#         cursor.close()
-#         db_connection.close()
-#         print("MySQL connection is closed")
+# # finally:
+# #     # Always close the connection
+# #     if 'db_connection' in locals() and db_connection.is_connected():
+# #         cursor.close()
+# #         db_connection.close()
+# #         print("MySQL connection is closed")
 
 
 
-# exp = {
-#     'taskid': 3,
-#     'taskname': 'examplee',
-#     'taskdescreption': 'example',
-#     'is_completed': False,
-# }
+# # exp = {
+# #     'taskid': 3,
+# #     'taskname': 'examplee',
+# #     'taskdescreption': 'example',
+# #     'is_completed': False,
+# # }
 
-# obj = create_task(task=exp)
+# # obj = create_task(task=exp)
 
-print(bool(1))
+# print(bool(1))
     
+
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+test=os.environ.get("Data_Base_config")
+test = eval(f"dict({test})")
+print(test)
+
+
+# def check_database_is_active()->bool:
+#       try:  
+#         # Establishing the connection
+#         db_connection = mysql.connector.connect(
+#             host="localhost",      # Standard for XAMPP
+#             user="root",           # Default XAMPP username
+#             password="ms9019",           # Default XAMPP password is empty
+#             database="To_do",
+#             auth_plugin="mysql_native_password" # Replace with your actual database name
+#         )
+    
+#         if db_connection.is_connected():
+#             return True ,db_connection
+            
+    
+#       except mysql.connector.Error as err:
+#           return False ,err
+    
+
+
+
+
